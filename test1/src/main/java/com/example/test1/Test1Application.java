25e6ca1369ebde811a49b2eab640e07fffb40ec5
zxm <2060197959@qq.com>
2019-05-05 14:54:24 +0800

    master commit2

--- test1/src/main/java/com/example/test1/Test1Application.java
+++ test1/src/main/java/com/example/test1/Test1Application.java
@@ -9,6 +9,7 @@ public class Test1Application {
 	public static void main(String[] args) {
 		System.out.println("master");
 		System.out.println("master1");
+		System.out.println("master2");
 		SpringApplication.run(Test1Application.class, args);
 	}
 
